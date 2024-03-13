a08d4ef8230060fc62180ec9b15d6b0b5d1ef308
Lucky Dee <luckydeto@gmail.com>
2024-03-13 05:53:27 -0400

    using J Miskov's shader, tweaked to work on newer lovr

--- main.lua
+++ main.lua
@@ -1,31 +1,44 @@
 require "camera"
-
+local point_light = require('pointlight')
+point_light.load(1024, vec3(0, 1, 0))  -- depth texture resolution & position
 lovr.graphics.setBackgroundColor(.1, .1, .1)
 
-function lovr.load()
 
+function lovr.load()
+ 
 end
 
 function lovr.update(dt)
   updateCamera(dt)
 end
+function drawScene(pass)
 
-function lovr.draw(pass)
-  drawCamera(pass)
-
-
-  -- Plane
-  local x, y, z = -0, 0, 0
-  local t = lovr.timer.getTime() * 1
   -- box
   local boxSize = 1
-  pass:setColor(.49, .34, .76)
+  pass:setColor(.3,.3,.3)
   pass:box(vec3(0, boxSize / 2, 0), vec3(boxSize))
 
   --box is like a filled in so using it instead of plane
-  pass:setColor(.94, .33, .31)
+  pass:setColor(0.8,0.8,0.8)
   pass:box(vec3(0, 0, 0), vec3(10, 0.01, 10))
 
+end
+function lovr.draw(pass)
 
-  pass:setShader()
+  
+  drawCamera(pass)
+  
+  --first draw , depth 
+  local depthPass = point_light.setPass()
+  drawScene(depthPass)
+  
+  --second draw, normal 
+  point_light.setShader(pass)
+  drawScene(pass)
+  
+  --assign point light settings
+  pass:sphere(mat4(point_light.position):scale(0.05))
+  point_light.position:set(0.4, 2, 0)
+
+  return lovr.graphics.submit({depthPass,pass})
 end
